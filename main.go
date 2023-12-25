6d8d78768e20074a1b92ebf37806a115dee2df20
Ion Dodon <iondodon2@gmail.com>
2023-12-25 21:46:39 +0200

    standalone function

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ type StreamedSlice[T any] []T
 
 type FunctionFunc[T any, R any] func(T) R
 
-type R any
-
-func (ss StreamedSlice[T]) trans(functionFunc FunctionFunc[T, R]) StreamedSlice[R] {
+func trans[T any, R any](ss StreamedSlice[T], functionFunc FunctionFunc[T, R]) StreamedSlice[R] {
 	var newSlice = make(StreamedSlice[R], 0)
 
 	for _, elem := range ss {
@@ -26,7 +24,7 @@ func (ss StreamedSlice[T]) collect() StreamedSlice[T] {
 func main() {
 	s := StreamedSlice[int]([]int{1, 2, 3})
 
-	res := s.trans(func(i int) R {
+	res := trans(s, func(i int) float32 {
 		return 2.21 + float32(i)
 	}).collect()
 
