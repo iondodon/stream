67b0456ad9c19a061de090aa8bcdce764a3e2092
Ion Dodon <iondodon2@gmail.com>
2023-12-25 15:14:27 +0200

    fixed the toSlice collector

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ type Action[T any] interface {
 }
 
 type consumer[T any] func(T) T
-type producer[T any] func() T
+type function[T any, R any] func(T) R
 
 func (c consumer[T]) Do(elem T) T {
 	return c(elem)
 }
 
-func (p producer[T]) Do(elem T) T {
-	return p()
+func (f function[T, R]) Do(elem T) R {
+	return f(elem)
 }
 
 type stream[T any] struct {
@@ -27,15 +27,24 @@ func (s *stream[T]) peek(action consumer[T]) *stream[T] {
 	return s
 }
 
+func (s *stream[T]) transform(action function[T, T]) *stream[T] {
+	s.actions = append(s.actions, action)
+	return &stream[T]{
+		collection: s.collection,
+		actions:    s.actions,
+	}
+}
+
 func (s *stream[T]) toSlice() []T {
-	var slice []T
-	for _, elem := range s.collection {
-		for _, action := range s.actions {
+	for _, action := range s.actions {
+		var slice []T
+		for _, elem := range s.collection {
 			result := action.Do(elem)
 			slice = append(slice, result)
 		}
+		s.collection = slice
 	}
-	return slice
+	return s.collection
 }
 
 func toStream[T any](collection []T) *stream[T] {
@@ -49,10 +58,8 @@ func main() {
 	list := []int{1, 2, 3}
 
 	s := toStream(list).
-		peek(func(e int) int {
-			fmt.Print(e, " ")
-			return e
-		}).
+		transform(func(i int) int { return i * 2 }).
+		peek(func(e int) int { fmt.Print(e, " "); return e }).
 		toSlice()
 
 	fmt.Println("\nResulting slice:", s)
