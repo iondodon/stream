9f380c73556a9f83d2dfc686a128eab4852fbfd5
Ion Dodon <iondodon2@gmail.com>
2023-12-25 17:01:46 +0200

    Use separate collections for types of actions

--- main.go
+++ main.go
@@ -2,47 +2,45 @@ package main
 
 import "fmt"
 
-type Action[T any] interface {
-	Do(elem T) T
+type action[T any] struct {
+	actionType string
 }
 
-type consumer[T any] func(T) T
-type function[T any, R any] func(T) R
-
-func (c consumer[T]) Do(elem T) T {
-	return c(elem)
+type peeker[T any] interface {
+	peek(elem T)
 }
 
-func (f function[T, R]) Do(elem T) R {
-	return f(elem)
+type consumerFunc[T any] func(T) T
+
+func (c consumerFunc[T]) peek(elem T) {
+	c(elem)
 }
 
 type stream[T any] struct {
 	collection []T
-	actions    []Action[T]
+
+	actions []action[T]
+
+	peekers   []peeker[T]
+	peekIndex int
 }
 
-func (s *stream[T]) peek(action consumer[T]) *stream[T] {
-	s.actions = append(s.actions, action)
+func (s *stream[T]) peek(actionFunc consumerFunc[T]) *stream[T] {
+	s.actions = append(s.actions, action[T]{actionType: "peek"})
+	s.peekers = append(s.peekers, actionFunc)
 	return s
 }
 
-func (s *stream[T]) transform(action function[T, T]) *stream[T] {
-	s.actions = append(s.actions, action)
-	return &stream[T]{
-		collection: s.collection,
-		actions:    s.actions,
-	}
-}
-
 func (s *stream[T]) toSlice() []T {
-	for _, action := range s.actions {
-		var slice []T
-		for _, elem := range s.collection {
-			result := action.Do(elem)
-			slice = append(slice, result)
+	for _, a := range s.actions {
+		if a.actionType == "peek" {
+			s.peekIndex = s.peekIndex + 1
+			if p, ok := s.peekers[s.peekIndex].(peeker[T]); ok {
+				for _, elem := range s.collection {
+					p.peek(elem)
+				}
+			}
 		}
-		s.collection = slice
 	}
 	return s.collection
 }
@@ -50,7 +48,11 @@ func (s *stream[T]) toSlice() []T {
 func toStream[T any](collection []T) *stream[T] {
 	return &stream[T]{
 		collection: collection,
-		actions:    nil,
+
+		actions: make([]action[T], 0),
+
+		peekers:   make([]peeker[T], 0),
+		peekIndex: -1,
 	}
 }
 
@@ -58,7 +60,6 @@ func main() {
 	list := []int{1, 2, 3}
 
 	s := toStream(list).
-		transform(func(i int) int { return i * 2 }).
 		peek(func(e int) int { fmt.Print(e, " "); return e }).
 		toSlice()
 
